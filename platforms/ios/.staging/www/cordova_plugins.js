7f8963af2d03b488bc62b4ce0782388288aef26f
Will White <WillWhite@MacBook-Pro-Image-2.local>
2014-03-09 21:05:57 -0400

    Initial iGoForth Commit
    All iGoForth files uploaded.

---
+++ platforms/ios/.staging/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.3.1",
+    "org.apache.cordova.splashscreen": "0.2.7",
+    "org.apache.cordova.dialogs": "0.2.6"
+}
+// BOTTOM OF METADATA
+});
